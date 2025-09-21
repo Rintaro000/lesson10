bcce2ddb09550b949a0c68318986c4ac52ba4574
甲斐凜太朗 <nupeshico76@gmail.com>
2025-09-21 11:27:48 +0900

    page1.pyを追加

---
+++ pages/page1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import random
+
+st.title("おまけ じゃんけんゲーム")
+
+st.write("最初はグー。じゃんけん...")
+
+janken_start = st.button("ポン！")
+
+if janken_start:
+    hand = random.randint(1,3)
+
+    if hand == 1:
+        st.write("グー")
+    
+    elif hand == 2:
+        st.write("チョキ")
+
+    else:
+        st.write("パー")
